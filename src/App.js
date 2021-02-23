c9d4a918ebd5df0fb86d4bb61c2344c63b6c9afd
Delia Drel <d.drel@hotmail.com>
2021-02-23 08:55:38 +0100

    -

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+
+import 'bulma/css/bulma.css'
+import Navbar from './navbar/Navbar.js'
+import FormField from './formfield/FormField.js'
+
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  // return <h1>Bulma</h1>;
+  return (
+    <React.Fragment>
+      <Navbar />
+      <FormField />
+    </React.Fragment>
+  )
 };
+
+ReactDOM.render(<App />, document.querySelector('#myApp'))
 
 export default App;
